3818352321187473f33034c22bcccf01fdec9009
yangh <yangh@j-cloud.cn>
2020-04-09 21:50:30 +0800

    基本样式和图标

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { IconStyle } from './assets/iconfont/iconfont'
+import { GlobalStyle } from './style'
 
 function App() {
   return (
